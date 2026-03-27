94e1a31803919872c460751902776f27962bb96b
mefju2 <55331075+mefju2@users.noreply.github.com>
2026-03-27 22:06:33 +0100

    feat: remove dark mode initialization from layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import '../app.css';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		document.documentElement.classList.add('dark');
-	});
+	import '$lib/stores/ui';
 </script>
 
 <slot></slot>
